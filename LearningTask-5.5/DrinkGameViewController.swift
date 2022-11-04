d6e00885e601185438dd471ab65a6f546b1033c7
Debora Rodrigues <debora.souza@zup.com.br>
2022-11-04 09:00:28 -0300

    Create GrinkGameViewController

--- LearningTask-5.5/DrinkGameViewController.swift
+++ LearningTask-5.5/DrinkGameViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DrinkGameViewController: UIViewController {
 
     @IBOutlet weak var drinkSorteadaLabel: UILabel!
     @IBOutlet weak var drinkAnteriorLabel: UILabel!
